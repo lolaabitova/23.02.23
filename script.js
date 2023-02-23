409427bf0899ac09efeb9739b976e6984b22a34a
Lola <lolaabitova90@gmail.com>
2023-02-23 12:25:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let example = document.querySelector("#example-id");
+example.textContent += "@" ;
+let example1 = document.querySelector("p");
+example1.textContent = "gdggfgfg" +  example1;textContent
+let example2 =document.querySelector(".example-class");
+example2.textContent += "@" ;
+let example3 = document.querySelector ("p:nth-child(4)");
+example3.textContent += "@" ;
